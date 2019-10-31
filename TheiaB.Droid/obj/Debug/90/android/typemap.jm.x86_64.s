85cc3c8a88521712da7492015ca539796f45a46b
Hector Martinez <hector.mtzm6@gmail.com>
2019-10-31 17:36:22 -0500

    initial push

---
+++ TheiaB.Droid/obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 00aca8b43134f1e971f193c55193e2fe7906ebfc */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1034
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 270909
+	.include	"typemap.jm.inc"
