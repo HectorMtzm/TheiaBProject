13f75e9b941f7d9060f3f1b52812e4a0df60cb1e
Hector Martinez <hector.mtzm6@gmail.com>
2019-11-01 00:18:03 -0500

    Added multiple views

--- TheiaB.Droid/MainActivity.cs
+++ TheiaB.Droid/MainActivity.cs
@@ -9,13 +9,29 @@ namespace TheiaB.Droid
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText userIdTextBox;
+        Button loginButton;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            userIdTextBox = FindViewById<EditText>(Resource.Id.userIdTextBox);
+            loginButton = FindViewById<Button>(Resource.Id.loginButton);
+
+            loginButton.Click += LoginButton_Click;
+
         }
+
+        private void LoginButton_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, $"Hello {userIdTextBox.Text}", ToastLength.Long).Show();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- TheiaB.Droid/obj/Debug/90/android/environment.armeabi-v7a.s
+++ TheiaB.Droid/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"01c47f23-e272-4581-a549-0a1d079f63e0"
+	.asciz	"69a2ab02-e922-48c2-9967-74a471c226b2"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
